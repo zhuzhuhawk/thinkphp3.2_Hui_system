3c6673a939e95b7e870fd19ab1876ab3e8e9a674
zhuzhuhawk <zhuzhuhawk@qq.com>
2017-05-31 21:32:45 +0800

    buycomefrom
    修改采购来源

--- Admin/Home/Controller/BuyController.class.php
+++ Admin/Home/Controller/BuyController.class.php
@@ -19,8 +19,24 @@ class BuyController extends Controller {
 		// p($m);
 		$this->display();
 	}
-
+	/**
+	 * 采购来源管理
+	 * ================
+	 * @
+	 * @DateTime  2017-05-31T21:28:08+0800
+	 * ================
+	 * @return    [type]                   [description]
+	 */
 	public function comefrom(){
+		 $md = M('buycomefrom');
+		$data = $md->select();
+		$countList = $md ->count();
+		$this->assign('data',$data);
+		// 统计数据条数
+		$this->countList = $countList;
+		// p($this);
+		// p($countList);
+
 		$this->display();
 	}
 
