1e6d1ddb8d6e0d6865f64fba82753bee101fc261
zhuzhuhawk <zhuzhuhawk@qq.com>
2017-06-21 08:45:25 +0800

    Staff模块增加
    staff模块增加

---
+++ Application/Home/Controller/StaffController.class.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Home\Controller;
+use Think\Controller;
+class StaffController extends Controller{
+	/**
+	 * 主页
+	 * ================
+	 * @AuthorHTL
+	 * @DateTime  2017-06-19T14:10:09+0800
+	 * ================
+	 * @return    [type]                   [description]
+	 */
+	public function index(){
+		$this->display();
+	}
+
+
+
+
+
+
+}
